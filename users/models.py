31d84b478e557e052d72da2eed07eb2ba42aed00
mofahmi99 <m.fahmy@sary.com>
2021-08-09 04:12:58 +0200

    User model updated by adding email,city and address fields

--- users/models.py
+++ users/models.py
@@ -7,10 +7,13 @@ from django.contrib.auth.base_user import AbstractBaseUser
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True, null=True, blank=True)
     phonenumber = models.CharField(max_length=255)
     token = models.CharField(max_length=255, null=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    city = models.ForeignKey('branches.City', on_delete=models.CASCADE, default=1)
+    address = models.ForeignKey('locations.Location', on_delete=models.CASCADE, default=1)
 
     USERNAME_FIELD = 'username'
     objects = UserManager()
